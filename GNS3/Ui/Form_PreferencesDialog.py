23bc74523b2b09482d216956b2c39a5bad7c4eca
Xavier ALT <dex@gns3.net>
2007-07-23 18:33:56 +0200

    [PATCH] Increase default size of PreferencesDialog

--- GNS3/Ui/Form_PreferencesDialog.py
+++ GNS3/Ui/Form_PreferencesDialog.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'Form_PreferencesDialog.ui'
 #
-# Created: Sun Jul 22 22:21:01 2007
+# Created: Mon Jul 23 16:02:29 2007
 #      by: PyQt4 UI code generator 4-snapshot-20070710
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,44 +12,61 @@ from PyQt4 import QtCore, QtGui
 class Ui_PreferencesDialog(object):
     def setupUi(self, PreferencesDialog):
         PreferencesDialog.setObjectName("PreferencesDialog")
-        PreferencesDialog.resize(QtCore.QSize(QtCore.QRect(0,0,419,304).size()).expandedTo(PreferencesDialog.minimumSizeHint()))
+        PreferencesDialog.resize(QtCore.QSize(QtCore.QRect(0,0,576,419).size()).expandedTo(PreferencesDialog.minimumSizeHint()))
+
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(2)
+        sizePolicy.setVerticalStretch(2)
+        sizePolicy.setHeightForWidth(PreferencesDialog.sizePolicy().hasHeightForWidth())
+        PreferencesDialog.setSizePolicy(sizePolicy)
+        PreferencesDialog.setModal(True)
 
         self.gridlayout = QtGui.QGridLayout(PreferencesDialog)
         self.gridlayout.setObjectName("gridlayout")
 
-        self.hboxlayout = QtGui.QHBoxLayout()
-        self.hboxlayout.setObjectName("hboxlayout")
-
         self.listWidget = QtGui.QListWidget(PreferencesDialog)
 
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum,QtGui.QSizePolicy.Expanding)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed,QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.listWidget.sizePolicy().hasHeightForWidth())
         self.listWidget.setSizePolicy(sizePolicy)
         self.listWidget.setMaximumSize(QtCore.QSize(160,16777215))
+
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        font.setWeight(75)
+        font.setBold(True)
+        self.listWidget.setFont(font)
         self.listWidget.setObjectName("listWidget")
-        self.hboxlayout.addWidget(self.listWidget)
+        self.gridlayout.addWidget(self.listWidget,0,0,1,1)
 
         self.vboxlayout = QtGui.QVBoxLayout()
+        self.vboxlayout.setSpacing(3)
         self.vboxlayout.setObjectName("vboxlayout")
 
-        self.label = QtGui.QLabel(PreferencesDialog)
+        self.titleLabel = QtGui.QLabel(PreferencesDialog)
+
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred,QtGui.QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.titleLabel.sizePolicy().hasHeightForWidth())
+        self.titleLabel.setSizePolicy(sizePolicy)
 
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setUnderline(False)
-        self.label.setFont(font)
-        self.label.setFrameShape(QtGui.QFrame.Box)
-        self.label.setFrameShadow(QtGui.QFrame.Plain)
-        self.label.setObjectName("label")
-        self.vboxlayout.addWidget(self.label)
+        self.titleLabel.setFont(font)
+        self.titleLabel.setFrameShape(QtGui.QFrame.Box)
+        self.titleLabel.setFrameShadow(QtGui.QFrame.Plain)
+        self.titleLabel.setObjectName("titleLabel")
+        self.vboxlayout.addWidget(self.titleLabel)
 
         self.stackedWidget = QtGui.QStackedWidget(PreferencesDialog)
 
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding,QtGui.QSizePolicy.MinimumExpanding)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
         sizePolicy.setHeightForWidth(self.stackedWidget.sizePolicy().hasHeightForWidth())
         self.stackedWidget.setSizePolicy(sizePolicy)
         self.stackedWidget.setObjectName("stackedWidget")
@@ -58,20 +75,19 @@ class Ui_PreferencesDialog(object):
         self.page.setObjectName("page")
         self.stackedWidget.addWidget(self.page)
         self.vboxlayout.addWidget(self.stackedWidget)
-        self.hboxlayout.addLayout(self.vboxlayout)
-        self.gridlayout.addLayout(self.hboxlayout,0,0,1,1)
+        self.gridlayout.addLayout(self.vboxlayout,0,1,1,1)
 
         self.line = QtGui.QFrame(PreferencesDialog)
         self.line.setFrameShape(QtGui.QFrame.HLine)
         self.line.setFrameShadow(QtGui.QFrame.Sunken)
         self.line.setObjectName("line")
-        self.gridlayout.addWidget(self.line,1,0,1,1)
+        self.gridlayout.addWidget(self.line,1,0,1,2)
 
         self.buttonBox = QtGui.QDialogButtonBox(PreferencesDialog)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Apply|QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.NoButton|QtGui.QDialogButtonBox.Ok|QtGui.QDialogButtonBox.Reset)
         self.buttonBox.setObjectName("buttonBox")
-        self.gridlayout.addWidget(self.buttonBox,2,0,1,1)
+        self.gridlayout.addWidget(self.buttonBox,2,0,1,2)
 
         self.retranslateUi(PreferencesDialog)
         QtCore.QObject.connect(self.buttonBox,QtCore.SIGNAL("accepted()"),PreferencesDialog.accept)
@@ -80,6 +96,6 @@ class Ui_PreferencesDialog(object):
         PreferencesDialog.setTabOrder(self.listWidget,self.buttonBox)
 
     def retranslateUi(self, PreferencesDialog):
-        PreferencesDialog.setWindowTitle(QtGui.QApplication.translate("PreferencesDialog", "Dialog", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("PreferencesDialog", "Prefs Label", None, QtGui.QApplication.UnicodeUTF8))
+        PreferencesDialog.setWindowTitle(QtGui.QApplication.translate("PreferencesDialog", "Preferences", None, QtGui.QApplication.UnicodeUTF8))
+        self.titleLabel.setText(QtGui.QApplication.translate("PreferencesDialog", "Prefs Label", None, QtGui.QApplication.UnicodeUTF8))
 
