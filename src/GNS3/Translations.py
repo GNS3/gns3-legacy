8ca3655af9b1bd1c3bec6d1a82ece2103a62394a
Xavier ALT <dex@gns3.net>
2007-09-26 17:49:44 +0200

    [I18N] Language loader now search in multiple directory.
    - Modified the Translator class to search in multiple directory.
      Also add some default user-dir:
        - Windows: %APPDATA%/gns3/Langs/
        - MacOS / Unix: ~/.gns3/Langs/

--- src/GNS3/Translations.py
+++ src/GNS3/Translations.py
@@ -20,7 +20,7 @@
 # Contact: contact@gns3.net
 #
 
-import os
+import os, sys
 import GNS3.Globals as globals
 from PyQt4 import QtCore, QtGui
 #from GNS3 import pkgdir
@@ -30,34 +30,83 @@ class Translator(object):
 
     def __init__(self):
 
-        self.__loadedLangs = {}
-        #self.__i18n_dir = os.path.join(pkgdir, "Langs")
-        self.__i18n_dir = os.path.dirname(os.path.abspath(GNS3.Langs.__file__))
+        self.__langs = {}
+        self.__langs_code = []
+        self.__lang_current = "" 
         self.__lastTranslator = None
 
-    def getAvailables(self):
+        # Set default i18n dir
+        self.__i18n_dir =  os.path.dirname(os.path.abspath(GNS3.Langs.__file__))
 
+        self.__i18n_dirs = [
+            os.path.dirname(os.path.abspath(GNS3.Langs.__file__))
+        ]
+        # Add user own i18n dir depending on platform.
+        if sys.platform[:3] == "win":
+            self.__i18n_dirs.append( os.environ["APPDATA"] + "\\gns3\\Langs\\" )
+        elif sys.platform.startswith('darwin') \
+            or sys.platform.startswith('linux'):
+            self.__i18n_dirs.append( os.environ["HOME"] + "/.gns3/Langs" )
+        
+        # Now find all available languages...
+        self.findAvailableLangs()
+
+
+    def findAvailableLangs(self):
         local_translator = QtCore.QTranslator()
+
+        # Get available languages
+        for i18n_dir in self.__i18n_dirs:
+            d = QtCore.QDir(i18n_dir)
+
+            if not d.exists():
+                continue
+
+            fileNames = d.entryList(QtCore.QStringList("*.qm"),
+                                    QtCore.QDir.Files, QtCore.QDir.Name)
+            for file in fileNames:
+                lang_filename = i18n_dir + str(d.separator().toAscii()) +  file
+                # Remove file prefix (Lang_)
+                lang = str(file)[5:]
+                # if extended notation used, take 5 chars an language code
+                if lang[2] == '_':
+                    lang_code = lang[:5]
+                # else only take 2 char (country code)
+                else:
+                    lang_code = lang[:2]
+
+                # Load the file to get the i18n language name
+                r_code = local_translator.load("Lang_" + lang_code, i18n_dir)
+                if r_code == False:
+                    # got error?
+                    continue
+                lang_name = unicode(local_translator.translate("MainWindow", "English"))
+
+                # Create/Update langs dictionnary
+                if self.__langs.has_key(lang_code):
+                    self.__langs[lang_code]['dirs'].append(i18n_dir)
+                else:
+                    self.__langs[lang_code] = {
+                        'code': lang_code,
+                        'name': lang_name,
+                        'dirs': [ i18n_dir ],
+                    }
+        
+        # Do some cleanup, and sort langs code
+        for (k,v) in self.__langs.iteritems():
+            # reverse directory order,
+            # so first dir = last found
+            v['dirs'].reverse()
+            self.__langs_code.append(k)
+        self.__langs_code.sort()
+
+
+    def getAvailables(self):
         lang_availables = []
 
-        trans_dir = QtCore.QDir(self.__i18n_dir)
-        fileNames = trans_dir.entryList(QtCore.QStringList("*.qm"), QtCore.QDir.Files, QtCore.QDir.Name)
-
-        for i in fileNames:
-            # Remove file prefix (Lang_)
-            lang = i[5:]
-            # if extended notation used, take 5 chars an language code
-            if lang[2] == '_':
-                lang_code = lang[:5]
-            # else only take 2 char (country code)
-            else:
-                lang_code = lang[:2]
-            
-            local_translator.load("Lang_" + lang, self.__i18n_dir)
-            lang_name = local_translator.translate("MainWindow", "English")
-
-            lang_availables.append([lang_code, lang_name])
-
+        for l in self.__langs_code:
+            d = self.__langs[l]
+            lang_availables.append([d['code'], d['name']])
         return (lang_availables)
 
     def switchLangTo(self, lang):
@@ -65,17 +114,27 @@ class Translator(object):
         if (len(lang) > 5):
             lang = lang[:5]
 
-        if self.__loadedLangs.has_key(lang):
-            pass    
+        if self.__lang_current == lang:
+            return
+        if not self.__langs.has_key(lang):
+            return
 
         translator = QtCore.QTranslator()
-        translator.load("Lang_" + lang, self.__i18n_dir)
+        r_code = False
+        for lang_dir in self.__langs[lang]['dirs']:
+            r_code = translator.load("Lang_" + lang, lang_dir)
+            if r_code == True:
+                break 
+        if r_code == False:
+            # TODO: Show a ERROR MsgBox
+            pass
 
         globals.GApp.installTranslator(translator)
         if self.__lastTranslator is not None:
             globals.GApp.removeTranslator(self.__lastTranslator)
 
         self.__lastTranslator = translator
+        self.__lang_current = lang
 
         for widget in globals.GApp.topLevelWidgets():
             try:
