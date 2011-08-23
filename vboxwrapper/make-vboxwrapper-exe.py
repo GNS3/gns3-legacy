f30acc8b024ab68e1e22459553e4d9c89e5ef959
Benjamin <marsil_b@epitech.eu>
2011-08-23 10:17:25 +0200

    Fix the headers for vim

--- vboxwrapper/make-vboxwrapper-exe.py
+++ vboxwrapper/make-vboxwrapper-exe.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+# vim: expandtab ts=4 sw=4 sts=4:
 # make-exe.py
 from distutils.core import setup
 import py2exe
--- vboxwrapper/vboxwrapper.py
+++ vboxwrapper/vboxwrapper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: expandtab ts=4 sw=4 sts=4:
 #
 # Copyright (c) 2007-2011 Thomas Pani, Jeremy Grossmann & Alexey Eromenko "Technologov"
 #
