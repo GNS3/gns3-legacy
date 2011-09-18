4ae7082daae9fffabc469978d7aa9babd662c256
Benjamin <marsil_b@epitech.eu>
2011-09-18 21:56:08 +0200

    Fix the script, pyuic4 path was hardcoded

--- src/GNS3/Ui/forms.sh
+++ src/GNS3/Ui/forms.sh
@@ -37,5 +37,6 @@ FILES=" Form_MainWindow
 for file in $FILES;
 do
     echo "Generating $file"
-    /Library/Frameworks/Python.framework/Versions/2.7/bin/pyuic4 "$file.ui" > "$file.py"
+    PATH=$PATH:/Library/Frameworks/Python.framework/Versions/2.7/bin/
+    pyuic4 "$file.ui" > "$file.py"
 done
