8ed0dd7403a6ae662286f58811eed44a1afe715e
grossmj <grossmj@gns3.net>
2013-10-22 18:25:23 -0600

    New py2exe script for vboxwrapper

--- vboxwrapper/make-vboxwrapper-exe.py
+++ vboxwrapper/make-vboxwrapper-exe.py
@@ -3,4 +3,4 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['vboxwrapper.py'], options = {"py2exe": {"typelibs": [('{46137EEC-703B-4FE5-AFD4-7C9BBBBA0259}',0,1,3)]}}, zipfile=None)
+setup(console=['vboxwrapper.py'], options = {"py2exe": {"dll_excludes": ["POWRPROF.dll", "MSWSOCK.dll"]}}, zipfile=None)
