4c0ebc4dc3ac2f7a97547a5c45c11e47bf095733
grossmj <grossmj@gns3.net>
2014-07-07 20:20:08 -0600

    Update Dynamips path in setup.py

--- setup.py
+++ setup.py
@@ -198,7 +198,7 @@ elif sys.platform.startswith('darwin'):
 
     print '*** Installing Dynamips ***'
     os.system('cp ../dynamips-0.2.8-RC3-community-OSX.intel64.bin ./GNS3.app/Contents/Resources')
-    os.system('cp ../dynamips-0.2.10-OSX.intel64.bin ./GNS3.app/Contents/Resources')
+    os.system('cp ../dynamips ./GNS3.app/Contents/Resources')
 
     print '*** Installing Qemu 0.11.0 ***'
     os.system('mkdir -p ./GNS3.app/Contents/Resources/Qemu-0.11.0')
