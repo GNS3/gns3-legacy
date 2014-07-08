d022581ad26e57f7b5f6054a930a5757153b31be
grossmj <grossmj@gns3.net>
2014-07-07 20:28:58 -0600

    One last change in setup.py

--- setup.py
+++ setup.py
@@ -220,7 +220,7 @@ elif sys.platform.startswith('darwin'):
     os.chmod('./GNS3.app/Contents/Resources/Qemu-0.14.1/bin/qemu-system-x86_64', 0755)
     os.chmod('./GNS3.app/Contents/Resources/Qemu-0.14.1/bin/qemu-img', 0755)
     os.chmod('./GNS3.app/Contents/Resources/dynamips-0.2.8-RC3-community-OSX.intel64.bin', 0755)
-    os.chmod('./GNS3.app/Contents/Resources/dynamips-0.2.10-OSX.intel64.bin', 0755)
+    os.chmod('./GNS3.app/Contents/Resources/dynamips', 0755)
     os.chmod('./GNS3.app/Contents/Resources/vpcs', 0755)
 
     print '*** Compiling & installing VBoxWrapper ***'
